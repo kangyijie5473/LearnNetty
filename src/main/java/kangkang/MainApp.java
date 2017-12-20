1782d65f9f57dcd5484d45366a86916f0c5d7efa
kangkang <xiyoulinux.kangyijie@gmail.com>
2017-12-20 18:38:17 +0800

    test jenkins

--- src/main/java/kangkang/MainApp.java
+++ src/main/java/kangkang/MainApp.java
@@ -14,7 +14,7 @@ public class MainApp {
         Main main = new Main();
         main.addRouteBuilder(new MyRouteBuilder());
         main.run(args);
-        System.out.println("jenkins");
+        System.out.println("ni da ye de");
     }
 
 }
